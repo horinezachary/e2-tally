9a8b24e202bed83f9e26cecfc82c902355811f07
ZacharyHorine <zachary@horine.dev>
2021-03-09 20:47:15 -0800

    remove prints

--- pine-gpio.js
+++ pine-gpio.js
@@ -22,27 +22,28 @@ class Pin {
     });
     exec('echo '+ direction +' > '+ gpio_path + 'gpio'+ number +"/direction",
     function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
+        //console.log('stdout: ' + stdout);
+        //console.log('stderr: ' + stderr);
         if (error !== null) {
              console.log('exec error: ' + error);
         }
     });
+    this.digitalWrite(0);
   }
 
   uninitialize(number) {
     exec('echo '+ 0 +' > '+ gpio_path + 'gpio'+ number +"/value",
     function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
+        //console.log('stdout: ' + stdout);
+        //console.log('stderr: ' + stderr);
         if (error !== null) {
              console.log('exec error: ' + error);
         }
     });
     exec('echo '+ number +' > '+ gpio_path + 'unexport',
     function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
+        //console.log('stdout: ' + stdout);
+        //console.log('stderr: ' + stderr);
         if (error !== null) {
              console.log('exec error: ' + error);
         }
@@ -53,8 +54,8 @@ class Pin {
     if (value == HIGH || value == LOW) {
       exec('echo '+ value +' > '+ gpio_path + 'gpio'+ this.pinNumber +"/value",
       function (error, stdout, stderr) {
-          console.log('stdout: ' + stdout);
-          console.log('stderr: ' + stderr);
+          //console.log('stdout: ' + stdout);
+          //console.log('stderr: ' + stderr);
           if (error !== null) {
                console.log('exec error: ' + error);
           }
